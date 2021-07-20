5171c967328481a438b15fe6aaaca06a562e7231
kei-600th <kei19941005@gmail.com>
2021-07-20 15:07:09 +0900

    トップページのフォーマット作成

--- commune_frontend/src/App.vue
+++ commune_frontend/src/App.vue
@@ -1,6 +1,24 @@
   <template>
     <div id="app">
-      <Top />
+      <v-app id="inspire">
+
+        <v-app-bar
+          app
+          shrink-on-scroll
+        >
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+          <v-toolbar-title>Application</v-toolbar-title>
+
+          <v-spacer></v-spacer>
+
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-app-bar>
+
+        <Top />
+      </v-app>
     </div>
   </template>
 
--- commune_frontend/src/components/Top.vue
+++ commune_frontend/src/components/Top.vue
@@ -1,27 +1,29 @@
 <template>
   <div>
-    <h1>top</h1>
-      <ul>
-        <li v-for="group in groups" :key="group.id">
-          {{ group.name }}
-        </li>
-      </ul>
-    <button type="submit" @click="fetchGroups()">fetchGroups</button>
+
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col
+            v-for="n in 24"
+            :key="n"
+            cols="4"
+          >
+            <v-card height="200"></v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
 
 
-    <TopDetails />
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
-import TopDetails from './TopDetails.vue';
 
 export default {
   name: 'Top',
-  components: {
-    TopDetails,
-  },
   computed: {
     ...mapGetters('groups', ['groups']),
   },
