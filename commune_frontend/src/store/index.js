05ff356175916133108ed825df4f2bbc3274cd5a
kei-600th <kei19941005@gmail.com>
2021-07-20 14:33:32 +0900

    vuexの利用

--- commune_frontend/src/store/index.js
+++ commune_frontend/src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import groups from './modules/groups';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules: {
+    groups
+  }
 });
