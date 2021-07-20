3ac0e12085049d2d20e33eb385e8f46d1db00d49
kei-600th <kei19941005@gmail.com>
2021-07-20 13:56:21 +0900

    axiosの導入

--- commune_frontend/src/App.vue
+++ commune_frontend/src/App.vue
@@ -1,39 +1,16 @@
+  <template>
+    <div id="app">
+      <Top />
+    </div>
+  </template>
 
-<template>
-  <v-app id="inspire">
-    <v-app-bar
-      app
-      shrink-on-scroll
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <script>
+  import Top from './components/Top.vue';
 
-      <v-toolbar-title>commune</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <v-container>
-        <v-row>
-          <v-col
-            v-for="n in 24"
-            :key="n"
-            cols="4"
-          >
-            <v-card height="200"></v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
-</template>
-
-<script>
   export default {
-    //
-  }
-</script>
+    name: 'App',
+    components: {
+      Top
+    }
+  };
+  </script>
---
+++ commune_frontend/src/components/Top.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>top</h1>
+    <p>groups:</p>
+    <p>{{ groups }}</p>
+    <button type="submit" @click="fetchGroups()">fetchGroups</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';  // axiosをインポートする
+
+export default {
+  name: 'Top',
+  data: () => ({
+    groups: []
+  }),
+  methods: {
+    fetchGroups() {
+      // GETリクエストを送信し、取得データをgroups変数に代入する
+      axios
+        .get('http://localhost:3000/groups')
+        .then(response => {
+          this.groups = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+    }
+};
+</script>
