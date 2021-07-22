5e7ba9cc9e0b69bb0cf540d12f7a3d1f8e68bfb9
kei-600th <kei19941005@gmail.com>
2021-07-22 17:56:57 +0900

    グループ作成用コンポーネント作成

---
+++ commune_frontend/src/components/GroupFormDialog.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card class="pb-12">
+    <v-card-actions class="d-flex justify-end pa-2">
+      <v-btn icon @click="closeDialog">
+        <v-icon size="20px">mdi-close</v-icon>
+      </v-btn>
+    </v-card-actions>
+    <v-card-text>
+        <v-text-field v-model="name" label="グループ名"></v-text-field>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: 'EventFormDialog',
+  data: () => ({
+    name: '',
+  }),
+  computed: {
+    ...mapGetters('groups', ['group']),
+  },
+  methods: {
+    ...mapActions('groups', ['setGroup']),
+    closeDialog() {
+      this.setGroup(null);
+    },
+  },
+};
+</script>
